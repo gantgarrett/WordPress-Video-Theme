1c248f50eed69d5d3d9dff90e10aa90d4583d1ed
garrett <garrett.compsci@gmail.com>
2022-10-21 08:54:13 -0700

    sidebar styling

--- sidebar.php
+++ sidebar.php
@@ -5,19 +5,26 @@
 
 if ( is_active_sidebar( 'primary_widget_area' ) || is_archive() || is_single() ) :
 ?>
-<div id="sidebar" class="sidenav bg-dark" style="width:250px">
+<div id="sidebar" class="sidenav bg-dark position-relative" style="width:250px">
+
+	<!-- Open state for side bar (default) -->
 	<div class="open-menu-wrapper" id="open-menu-id" style="display:block;">
 		<?php wp_nav_menu( array( 
 			'menu' => 'Sidebar open menu', 
-			'container_class' => 'sidebar-open-menu', 
+			'container_class' => 'sidebar-open-menu position-absolute w-100', 
+			'menu_class'     => 'open-menu m-0 ps-3 list-unstyled text-primary',
 		)); ?>
 	</div>
+
+	<!-- Closed state for side bar -->
 	<div class="closed-menu-wrapper" id="closed-menu-id" style="display:none;">
 		<?php wp_nav_menu( array( 
 			'menu' => 'Sidebar closed menu', 
-			'container_class' => 'sidebar-closed-menu', 
+			'container_class' => 'sidebar-closed-menu position-absolute',
+			'menu_class'     => 'closed-menu m-0 ps-3 list-unstyled text-primary', 
 		)); ?>
 	</div>
+
 </div><!-- /#sidebar -->
 <?php
 	endif;
