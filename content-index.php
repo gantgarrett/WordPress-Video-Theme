68d1ddc709db42d8f5c2889abfedfa63ef73ce65
garrett <garrett.compsci@gmail.com>
2022-11-03 10:25:11 -0700

    thumbnail aspect ratio

--- content-index.php
+++ content-index.php
@@ -5,8 +5,10 @@
 ?>
 <div class="col-lg-3 col-md-4 d-flex">
 	<div class="card d-flex flex-column align-items-stretch mb-3 px-2 w-100">
-		<div class="card-img-container">
-			<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" class="card-img-top video-thumbnail" alt="Hollywood Sign on The Hill" />
+		<div class="img-container">
+			<div class="card-img-container">
+				<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" class="card-img-top video-thumbnail" alt="Hollywood Sign on The Hill" />
+			</div>
 		</div>
 		<div class="card-body">
 			<h5 class="card-title">Card title</h5>
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $page_id = get_option( 'page_for_posts' );
 <div class="container p-0 d-flex flex-column">
 	<?php get_template_part('partials/content', 'catsCarousel'); ?>
 	<div id="main-content" class="container d-flex flex-wrap ps-5 pt-5 js-filter" style="padding-right:18rem;">
-		<div class="row row-cols-4 align-items-start post-wrapper g-2">
+		<div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-12 align-items-start post-wrapper g-2">
 			<?php
 				get_template_part( 'archive', 'loop' );
 			?>
