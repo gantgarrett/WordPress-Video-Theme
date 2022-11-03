855beaf73c1214bfe572c5234c3abb65f4330581
garrett <garrett.compsci@gmail.com>
2022-11-03 14:21:26 -0700

    added category page layout

--- category.php
+++ category.php
@@ -5,20 +5,19 @@ Template Name: Category Archives
 
 get_header();
 
-$page_id = get_option( 'page_for_posts' );
 ?>
 
 <?php get_template_part( 'sidebar' ); ?>
 
-<div class="container-fluid p-0 d-flex flex-column">
+<div class="container p-0 d-flex flex-column">
 	<?php get_template_part('partials/content', 'catsCarousel'); ?>
-	<div id="main-content" class="row js-filter">
-		<div class="col-md-12">
+	<div id="main-content" class="container d-flex flex-wrap ps-5 pt-5 js-filter" style="padding-right:18rem;">
+		<div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-12 align-items-start post-wrapper g-2">
 			<?php
 				get_template_part( 'archive', 'loop' );
 			?>
-		</div><!-- /.col -->
-	</div><!-- /.row -->
+		</div>
+	</div>
 </div>
 
 <?php
