49dd641245e4f09e753676dec784c146fd82e73f
garrett <garrett.compsci@gmail.com>
2022-11-09 12:30:26 -0700

    sidebar single toggler

--- assets/js/sidebar-single.js
+++ assets/js/sidebar-single.js
@@ -1,4 +1,5 @@
 function sideBarSingleToggle() {
     var sidebarSingle = document.getElementById('sidebar-single');
     sidebarSingle.style.display = sidebarSingle.style.display == 'block' ? 'none' : 'block';
+    sidebarSingle.style.width = sidebarSingle.style.width == '250px' ? '0' : '250px';
 }
--- sidebar-single.php
+++ sidebar-single.php
@@ -1,3 +1,3 @@
-<div id="sidebar-single" class="sidebar-single" style="display:none;">
+<div id="sidebar-single" class="sidebar-single position-fixed h-100 w-8 bg-dark" style="display:none;">
 	<?php dynamic_sidebar( 'secondary_widget_area' ); ?>
 </div>
