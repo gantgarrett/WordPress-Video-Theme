0dcb6e393f5f98054891d27ab81f3497097f3d07
garrett <garrett.compsci@gmail.com>
2022-11-10 12:24:07 -0700

    single page author meta styling

--- content-single.php
+++ content-single.php
@@ -56,10 +56,66 @@
 						get_template_part( 'author', 'bio' );
 					?>
 				</div><!-- /.entry-meta -->
-				<div class="post-title">
-					<h1 class="display-6">
-						<small><?php the_title(); ?></small>
+				<div class="post-title-container">
+					<h1 class="post-title">
+						<?php the_title(); ?>
 					</h1>
+				</div>
+				<div class="author-meta container-fluid px-0 mt-3">
+					<div class="row align-items-center">
+						<div class="col-6">
+							<div class="row align-items-center">
+								<div class="col-auto">
+									<?php 
+										$author_id = get_post_field('post_author', $post->ID);
+										$output = get_avatar_url($author_id, ['size' => '42']);
+										echo '<img class="rounded-circle" src="'.$output.'"/>';
+									?>
+								</div>
+								<div class="col-auto ps-0 pe-4">
+									<div class="row d-flex flex-column">
+										<div class="col">
+											<h5>
+												<?php the_author_meta('display_name'); ?>
+											</h5>
+										</div>
+										<div class="col">
+											<small class="text-muted">1.1M Subscribers</small>
+										</div>
+									</div>
+								</div>
+								<div class="col">
+									<button type="button" class="badge rounded-pill bg-danger border-0 py-2 px-3">
+										<h6>Subscribe</h6>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-6 px-0">
+							<div class="row justify-content-end">
+								<div class="col-auto px-1 d-flex justify-content-end">
+									<button type="button" class="badge rounded-pill bg-secondary border-0 py-2 px-3">
+										<h6>Like/Dislike</h6>
+									</button>
+								</div>
+								<div class="col-auto px-1 d-flex justify-content-end">
+									<button type="button" class="badge rounded-pill bg-secondary border-0 py-2 px-3">
+										<h6>Share</h6>
+									</button>
+								</div>
+								<div class="col-auto px-1 d-flex justify-content-end">
+									<button type="button" class="badge rounded-pill bg-secondary border-0 py-2 px-3">
+										<h6>Save</h6>
+									</button>
+								</div>
+								<div class="col-auto px-1 d-flex justify-content-end">
+									<button type="button" class="badge rounded-pill bg-secondary border-0 py-2 px-3">
+										<h6>...</h6>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</article><!-- /#post-<?php the_ID(); ?> -->
 		</div>
